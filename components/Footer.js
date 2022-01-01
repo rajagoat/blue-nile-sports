5418e773e6a0505f4dc5e6e4ff5f15ca9ddfb3cf
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-12-31 17:56:01 -0700

    created base header and footer

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="logo">
+                <p>Blue Nile Sports</p>
+            </div>
+            <p>Copyright 2021</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Blue Nile Sports</h1>
+            </div>
+            <a href="#">Articles</a>
+            <a href="#">Player Profiles</a>
+            <a href="#">Schedule</a>
+            <a href="#">Live Feed</a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
