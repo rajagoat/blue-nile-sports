6f4b5347ac3dd0c76d2cfb7fabead33d5e180464
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-01-04 02:26:06 -0700

    Articles are now displayed

--- components/ArticleSnippet.js
+++ components/ArticleSnippet.js
@@ -1,23 +1,26 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/ArticleSnippet.module.css'
 
-const ArticleSnippet = ({ article: { title, author, dateFormatted, body, thumbnail } }) => {
+const ArticleSnippet = ({ article: { slug, title, author, dateFormatted, body, thumbnail } }) => {
     const truncate = (str, no_words) => {
         return str.split(" ").splice(0, no_words).join(" ") + "...";
     }
 
     return (
-        <article className={styles.box}>
-            <div className={styles.mediaContent}>
-                <h2 className={styles.mediaTitle}>{title}</h2>
-                <span className={styles.mediaAuthor}>{author} · </span>
-                <span className={styles.mediaDate}>{dateFormatted}</span>
-                <p className={styles.mediaBody}>{truncate(body, 10)}</p>
-            </div>
-            <>
-                <Image className={styles.image} src={thumbnail} width={200} height={110} layout='intrinsic'/>
-            </>
-        </article>
+        <Link href={`/articles/${slug}`}>
+            <article className={styles.box}>
+                <div className={styles.mediaContent}>
+                    <h2 className={styles.mediaTitle}>{title}</h2>
+                    <span className={styles.mediaAuthor}>{author} · </span>
+                    <span className={styles.mediaDate}>{dateFormatted}</span>
+                    <p className={styles.mediaBody}>{truncate(body, 10)}</p>
+                </div>
+                <>
+                    <Image className={styles.image} src={thumbnail} width={200} height={110} layout='intrinsic' />
+                </>
+            </article>
+        </Link>
     );
 }
 
---
+++ pages/articles/[slug].js
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import articles from '../../public/data/articles.json'
+import styles from '../../styles/ArticleDetails.module.css'
+
+export const getStaticPaths = async () => {
+    return {
+        paths: articles.map(article => ({ params: { slug: article.slug } })),
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    return {
+        props: {
+            article: articles.find(article => article.slug === context.params.slug)
+        }
+    }
+}
+
+const Details = ({ article: { thumbnail, title, author, dateFormatted, bodyHtml } }) => {
+    return (
+        <div className={styles.page}>
+            <div className={styles.imageContainer}>
+                <Image className={styles.image} src={thumbnail} layout='fill' priority />
+            </div>
+            <div className={styles.details}>
+                <div className={styles.headlineContainer}>
+                    <h1 className={styles.title}>{title}</h1>
+                    <span className={styles.author}>{author} · </span>
+                    <span className={styles.dateFormatted}>{dateFormatted}</span>
+                </div>
+                <div className={styles.content} dangerouslySetInnerHTML={{ __html: bodyHtml }} />
+            </div>
+        </div>
+    );
+}
+
+export default Details;
