ad5be8ec16573fc4b80ec2fbde72c4d82a2f9fad
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-01-04 03:29:12 -0700

    footer stays at bottom of page now

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.main}>
+    <div>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima mollitia optio, nihil laudantium explicabo animi sapiente. Sunt a error voluptates accusantium quis explicabo quod recusandae accusamus. Quisquam vero velit perferendis.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo at doloribus dignissimos adipisci est repellat quia et officiis? Repudiandae minima accusantium pariatur commodi quos dolorum veniam aperiam sit ipsa?</p>
