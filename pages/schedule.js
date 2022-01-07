46fbd5fc0b7e9c626d4185fcbd070fb175f4a53f
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-01-07 00:55:09 -0700

    Schedule complete

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,3 +1,8 @@
+import styles from '../styles/Schedule.module.css'
+import homeBackground from '../public/images/static/home.svg'
+import awayBackground from '../public/images/static/away.svg'
+import Image from 'next/image';
+
 export const getStaticProps = async () => {
     const fixturesAPIp1 = 'https://livescore-api.com/api-client/fixtures/matches.json?&key=gHEJyJ4QGSqIuUPX&secret=8ksmR4idpMVsioSz6xiEhnvXfWGKRvHZ&competition_id=227';
     const fixturesAPIp2 = 'https://livescore-api.com/api-client/fixtures/matches.json?&key=gHEJyJ4QGSqIuUPX&secret=8ksmR4idpMVsioSz6xiEhnvXfWGKRvHZ&competition_id=227&page=2';
@@ -9,6 +14,14 @@ export const getStaticProps = async () => {
     // concatenates all fixtures
     data1.data.fixtures = data1.data.fixtures.concat(data2.data.fixtures);
 
+    // changes dates to readable versions
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    data1.data.fixtures.map(fixture => {
+        let info = fixture.date.split('-');
+        let newDate = `${months[Number(info[1]) - 1]} ${info[2]} ${info[0]}`;
+        fixture.date = newDate
+    })
+
     return {
         props: { matches: data1 }
     }
@@ -16,23 +29,34 @@ export const getStaticProps = async () => {
 
 
 
-const Schedule = ({ matches: {success, data: {fixtures}} }) => {    
+const Schedule = ({ matches: { success, data: { fixtures } } }) => {
     return (
         <div>
-            <h1>Schedule</h1>
-            {success && <div className="container">
-                {fixtures.map(fixture => 
-                    <div className="fixture" key={fixture.id}>
-                        {fixture.home_name}
-                        {fixture.away_name}
-                        {fixture.date}
-                        {fixture.time}
-                        {fixture.location}
+            {success && <div className={styles.container}>
+                {fixtures.map(fixture =>
+                    <div className={styles.fixture} key={fixture.id}>
+                        <div className={styles.home}>
+                            <Image src={homeBackground} width={250} height={200} layout='fixed' />
+                            <h2>{fixture.home_name}</h2>
+                        </div>
+                        <div className={styles.matchDetails}>
+                            <h2>{fixture.date}</h2>
+                            <h3>{fixture.time}</h3>
+                            {fixture.location ?
+                                <p>{fixture.location}</p>
+                                :
+                                <p>Not Available</p>
+                            }
+                        </div>
+                        <div className={styles.away}>
+                            <Image src={awayBackground} width={250} height={210} layout='fixed' />
+                            <h2>{fixture.away_name}</h2>
+                        </div>
                     </div>
                 )}
             </div>}
         </div>
     );
 }
- 
+
 export default Schedule;
