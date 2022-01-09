3277123443a4912e5e6ab80b62c80d9cb36e8943
noelfranthomas <noelfranthomas@gmail.com>
2022-01-09 15:13:27 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,14 +9,14 @@ class MainDocument extends Document {
   render() {
     return (
       <Html>
-        {/* <Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <Head>
+          {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000" />
-          <meta name="theme-color" content="#000" />
-        </Head> */}
+          <meta name="theme-color" content="#000" /> */}
+        </Head>
         <body>
           <Main />
           <NextScript />
