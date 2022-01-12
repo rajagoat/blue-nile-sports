4a1620eee0da514aae3b4d9ba72f079b8091caac
noelfranthomas <noelfranthomas@gmail.com>
2022-01-11 17:21:48 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -12,12 +12,14 @@ const Footer = () => {
         <footer className={styles.wrapper}>
             <h1>Blue Nile Sport</h1>
             <div>
-                <a target="_blank" href='https://www.instagram.com/bluenilesports/?hl=en' rel="noopener noreferrer">
                     <div style={{margin:"16px 0", display:"flex", justifyContent:"space-evenly", width: "100px"}}>
+                    <a target="_blank" href='https://www.instagram.com/bluenilesports/?hl=en' rel="noopener noreferrer">
                         <Image src={instagramIcon} alt="instagram page" height={25} width={25} />
-                        <Image src={twitterIcon} alt="instagram page" height={25} width={25} />
+                    </a>
+                    <a target="_blank" href="https://twitter.com/BlueNileSport" rel="noopener noreferrer">
+                        <Image src={twitterIcon} alt="twitter page" height={25} width={25} />
+                    </a>
                     </div>
-                </a>
             </div>
             <p>Copyright &copy; {date}</p>
         </footer>
